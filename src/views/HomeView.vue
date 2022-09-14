7d7f4931f2ad18061e3ad7a258bc9fbf2a8dc95f
Florent gravin <florent.gravin@camptocamp.com>
2022-09-14 16:37:02 +0200

    refactor: MapButton -> UiButton

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -9,7 +9,9 @@ import HelloWorld from '../components/HelloWorld.vue'
     <div class="w-72 p-4 bg-blue-300"></div>
     <div class="grow p-4 relative">
       <HelloWorld msg="The home" />
-      <TransportButtons class="absolute right-64 top-0"></TransportButtons>
+      <div class="absolute right-2 top-2">
+        <TransportButtons></TransportButtons>
+      </div>
     </div>
   </main>
 </template>
