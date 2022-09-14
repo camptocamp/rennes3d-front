e13a0b5d0c225eda84ca2b9d56793c9676b2691c
Florent gravin <florent.gravin@camptocamp.com>
2022-09-14 20:37:15 +0200

    feat(histoire): setup the first stories

---
+++ src/components/ui/UiButton.story.vue
@@ -0,0 +1,49 @@
+<script lang="ts" setup>
+import UiButton from './UiButton.vue'
+
+function initState() {
+  return {
+    disabled: false,
+    active: false,
+  }
+}
+</script>
+
+<template>
+  <Story
+    title="UiButton"
+    :layout="{
+      type: 'grid',
+      width: 200,
+    }"
+  >
+    <Variant title="Text Button" :init-state="initState">
+      <template #default="{ state }">
+        <UiButton :disabled="state.disabled" :active="state.active"
+          >Click
+        </UiButton>
+      </template>
+    </Variant>
+
+    <Variant title="Icon Button" :init-state="initState" icon="lucide:camera">
+      <template #default="{ state }">
+        <UiButton :disabled="state.disabled" :active="state.active">
+          <svg width="32" height="32" viewBox="0 0 24 24">
+            <g
+              fill="none"
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+            >
+              <path
+                d="M14.5 4h-5L7 7H4a2 2 0 0 0-2 2v9a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2h-3l-2.5-3z"
+              />
+              <circle cx="12" cy="13" r="3" />
+            </g>
+          </svg>
+        </UiButton>
+      </template>
+    </Variant>
+  </Story>
+</template>
--- src/components/ui/UiButton.vue
+++ src/components/ui/UiButton.vue
@@ -1,12 +1,22 @@
-<script setup lang="ts">
-const props = defineProps({
-  active: Boolean,
+<script lang="ts" setup>
+defineProps({
+  active: {
+    type: Boolean,
+    default: false,
+  },
+
+  disabled: {
+    type: Boolean,
+    default: false,
+  },
 })
 </script>
+
 <template>
   <button
+    :disabled="disabled"
     class="shadow-lg px-4 py-4 rounded-full"
-    :class="{ 'bg-blue-500': props.active, 'text-white': props.active }"
+    :class="{ 'bg-blue-500': active, 'text-white': active }"
   >
     <slot></slot>
   </button>
