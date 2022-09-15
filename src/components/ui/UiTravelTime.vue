5e5aa5952494217365c936e2a4ba91a1cf626f61
Ismail Sunni <imajimatika@gmail.com>
2022-09-15 17:19:43 +0700

    Feat: Add basic UiTravelTime component and its story.

---
+++ src/components/ui/UiTravelTime.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+const props = defineProps({
+  newDuration: Number,
+  oldDuration: Number,
+  lineNumber: String,
+  startStation: String,
+  endStation: String,
+})
+</script>
+<template>
+  <span>
+    <div>{{ props.newDuration }} min</div>
+    <div>au lie de {{ props.oldDuration }}</div>
+  </span>
+  <span> {{ props.lineNumber }} </span>
+  <span> {{ props.startStation }}</span>
+  <span> {{ props.endStation }}</span>
+  <div class="w-32 h-24"></div>
+</template>
