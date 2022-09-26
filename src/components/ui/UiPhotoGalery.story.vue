499224bdb69d880cf95886ebf3c2009125268e1d
Ismail Sunni <imajimatika@gmail.com>
2022-09-26 15:14:03 +0200

    Add basic component and its story.

---
+++ src/components/ui/UiPhotoGalery.story.vue
@@ -0,0 +1,16 @@
+<script lang="ts" setup>
+import UiPhotoGalery from './UiPhotoGalery.vue'
+</script>
+
+<template>
+  <Story
+    title="UiPhotoGalery"
+    :layout="{
+      type: 'grid',
+      width: 500,
+    }"
+    group="homepage"
+  >
+    <UiPhotoGalery></UiPhotoGalery>
+  </Story>
+</template>
---
+++ src/components/ui/UiPhotoGalery.vue
@@ -0,0 +1,5 @@
+<script setup lang="ts"></script>
+<template>
+  <div>show/hide button</div>
+  <div>3 Photos here</div>
+</template>
