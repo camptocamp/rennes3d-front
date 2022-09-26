6cbbe958566b34af42dfea31f53b7f416c7d3086
Ismail Sunni <imajimatika@gmail.com>
2022-09-26 12:21:13 +0200

    Add basic component for Tram Logo.

---
+++ src/components/ui/UiTrambusLogo.story.vue
@@ -0,0 +1,15 @@
+<script lang="ts" setup>
+import UiTrambusLogo from './UiTrambusLogo.vue'
+</script>
+
+<template>
+  <Story
+    title="UiTrambusLogo"
+    :layout="{
+      type: 'grid',
+      width: 400,
+    }"
+  >
+    <UiTrambusLogo></UiTrambusLogo>
+  </Story>
+</template>
---
+++ src/components/ui/UiTrambusLogo.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div>
+    <div>Logo</div>
+    <div>Reseau Trambus</div>
+  </div>
+  <div>
+    Découvrez et contribuez au futur réseau de transport de Rennes Métropole
+  </div>
+</template>
