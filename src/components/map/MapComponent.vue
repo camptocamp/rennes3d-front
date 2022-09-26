3332c782f7a2c0b428749ed900950c88572d2e4a
Florent gravin <florent.gravin@camptocamp.com>
2022-09-26 18:42:21 +0200

    refactor(map): simplify vcmap workflow

--- src/components/map/MapComponent.vue
+++ src/components/map/MapComponent.vue
@@ -1,61 +1,37 @@
 <script setup lang="ts">
 import { useLayersStore } from '@/stores/layers'
 import type { MapCollection } from '@vcmap/core'
-import { onMounted } from 'vue'
-import setup from '../../services/map'
+import { onMounted, Ref, ref } from 'vue'
+import { prepareContext } from '../../services/vcmap/context'
+import UiMap from '../ui/UiMap.vue'
 
-defineProps({
-  is3D: {
-    type: Boolean,
-    default: false,
-  },
-})
-let mapCollection2D: MapCollection
-// let mapCollection3D: MapCollection
-
+let mapCollection: Ref<MapCollection | null> = ref(null)
 const layerStore = useLayersStore()
 
-onMounted(() => {
-  setup().then((r) => {
-    mapCollection2D = r.mapCollection2D
-    // mapCollection3D = r.mapCollection3D
-  })
+onMounted(async () => {
+  mapCollection.value = await prepareContext()
 })
 
-function setLayerVisible(
-  mapCollection: MapCollection,
-  layerName: string,
-  visible: boolean
-) {
+function setLayerVisible(layerName: string, visible: boolean) {
   if (visible) {
-    mapCollection.layerCollection.getByKey(layerName).activate()
+    mapCollection.value?.layerCollection.getByKey(layerName).activate()
   } else {
-    mapCollection.layerCollection.getByKey(layerName).deactivate()
+    mapCollection.value?.layerCollection.getByKey(layerName).deactivate()
   }
 }
 layerStore.$subscribe(() => {
-  setLayerVisible(mapCollection2D, 'metro', layerStore.visibilities.metro)
-  setLayerVisible(mapCollection2D, 'bus', layerStore.visibilities.bus)
-  setLayerVisible(mapCollection2D, 'bike', layerStore.visibilities.bike)
+  setLayerVisible('metro', layerStore.visibilities.metro)
+  setLayerVisible('bus', layerStore.visibilities.bus)
+  setLayerVisible('bike', layerStore.visibilities.bike)
 })
 </script>
 <template>
-  <div
-    id="map2D"
-    class="overflow-hidden flex-1 bg-black h-full"
-    :class="{ hidden: is3D }"
-  ></div>
-  <div
-    id="map3D"
-    class="overflow-hidden flex-1 bg-black h-full"
-    :class="{ hidden: !is3D }"
-  ></div>
+  <div class="overflow-hidden flex-1 bg-black h-full">
+    <UiMap :map="mapCollection"></UiMap>
+  </div>
 </template>
 
 <style scoped>
-#map2D :deep(.mapElement) {
-  height: 100%;
-}
 #map3D :deep(.mapElement) {
   height: 100%;
 }
