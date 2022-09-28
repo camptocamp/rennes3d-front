17bda78101deb415b8a09eee138cfe87757ed0cf
Ismail Sunni <imajimatika@gmail.com>
2022-09-28 07:48:55 +0200

    Use font-poppins in higher level.

--- src/components/ui/UiTrambusLogo.vue
+++ src/components/ui/UiTrambusLogo.vue
@@ -3,18 +3,16 @@ import trambusLogo from '@/assets/icons/trambusLogo.png'
 </script>
 
 <template>
-  <div class="flex flex-col items-start p-0 gap-2 w-4/5 h-28">
+  <div class="flex flex-col items-start p-0 gap-2 w-4/5 h-28 font-poppins">
     <div class="flex flex-row items-start p-0 gap-2 h-14">
       <div class="h-14 w-14 flex flex-col justify-center">
         <img :src="trambusLogo" />
       </div>
-      <div
-        class="font-poppins font-bold text-4xl w-80 h-14 justify-center flex flex-col"
-      >
+      <div class="font-bold text-4xl w-80 h-14 justify-center flex flex-col">
         Reseau Trambus
       </div>
     </div>
-    <div class="font-poppins font-light text-base leading-6">
+    <div class="font-light text-base leading-6">
       Découvrez et contribuez au futur réseau de transport de Rennes Métropole
     </div>
   </div>
