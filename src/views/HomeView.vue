69b364253cbe3ea1a51bc2106062d3b1a53fe5ba
Ismail Sunni <imajimatika@gmail.com>
2022-09-29 03:41:52 +0200

    Put all existing components on homeview (still need some tweak on style).

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,16 +4,16 @@ import PhotoGallery from '../components/home/PhotoGallery.vue'
 import MapComponent from '../components/map/MapComponent.vue'
 import LineDescriptions from '../components/home/LineDescriptions.vue'
 import TravelTimes from '../components/home/TravelTimes.vue'
+import UiTrambusTitle from '../components/ui/UiTrambusTitle.vue'
 </script>
 
 <template>
   <main class="h-screen flex">
-    <div class="w-72 p-4 bg-blue-300">
-      <div class="">
-        <NetworkFigures></NetworkFigures>
-        <div><TravelTimes></TravelTimes></div>
-        <div><LineDescriptions></LineDescriptions></div>
-      </div>
+    <div class="w-96 p-4 bg-blue-300 flex flex-col">
+      <UiTrambusTitle></UiTrambusTitle>
+      <NetworkFigures></NetworkFigures>
+      <TravelTimes></TravelTimes>
+      <LineDescriptions></LineDescriptions>
     </div>
     <div class="flex grow relative">
       <MapComponent></MapComponent>
