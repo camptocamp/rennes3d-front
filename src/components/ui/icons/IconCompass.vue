524edfe4307beade2f7d22bf4670637febadc238
Abdelmalek <ab.khelouat@gmail.com>
2022-10-11 09:06:56 +0200

    feat(ui): add compass svg icon

---
+++ src/components/ui/icons/IconCompass.vue
@@ -0,0 +1,24 @@
+<template>
+  <svg
+    version="1.1"
+    id="Capa_1"
+    xmlns="http://www.w3.org/2000/svg"
+    xmlns:xlink="http://www.w3.org/1999/xlink"
+    x="0px"
+    y="0px"
+    viewBox="0 0 488.98 488.98"
+    style="enable-background: new 0 0 488.98 488.98"
+    xml:space="preserve"
+    width="1.5rem"
+    height="1.5rem"
+  >
+    <g>
+      <g>
+        <path
+          d="M409.49,485.75l-165-80.9l-165,80.9c-25.3,12.4-52.4-13.1-41.6-39.1l178.5-427.9c10.4-25,45.8-25,56.3,0l178.4,427.9
+			C461.89,472.55,434.79,498.15,409.49,485.75z"
+        />
+      </g>
+    </g>
+  </svg>
+</template>
