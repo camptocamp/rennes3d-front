fe4736af1efe36b6ec3c73df7fc61948138f2306
Ismail Sunni <imajimatika@gmail.com>
2022-10-11 09:52:13 +0200

    Add Rennes logo and line description subtitle.

---
+++ src/components/ui/UiRennesLogo.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup>
+import rennesLogo from '@/assets/icons/rennesLogo.png'
+</script>
+
+<template>
+  <div class="h-8 w-32 flex flex-col justify-center">
+    <img :src="rennesLogo" />
+  </div>
+</template>
