652d1ccc801c2942081b9b8695063953fd6a1104
Abdelmalek <ab.khelouat@gmail.com>
2022-10-12 23:35:52 +0200

    feat(ui): add timeline story

---
+++ src/components/ui/UiTimeline.story.vue
@@ -0,0 +1,74 @@
+<script lang="ts" setup>
+import UiTimeline from './UiTimeline.vue'
+
+function initState() {
+  return {
+    keydates: [
+      {
+        name: 'Semestre 1',
+        year: 2025,
+      },
+      {
+        name: 'Semestre 2',
+        year: 2025,
+      },
+      {
+        name: 'Semestre 1',
+        year: 2026,
+      },
+      {
+        name: 'Semestre 2',
+        year: 2026,
+      },
+      {
+        name: 'Semestre 1',
+        year: 2027,
+      },
+      {
+        name: 'Semestre 2',
+        year: 2027,
+      },
+      {
+        name: 'Semestre 1',
+        year: 2028,
+      },
+      {
+        name: 'Semestre 2',
+        year: 2028,
+      },
+      {
+        name: 'Semestre 1',
+        year: 2029,
+      },
+      {
+        name: 'Semestre 2',
+        year: 2029,
+      },
+      {
+        name: 'Semestre 1',
+        year: 2030,
+      },
+      {
+        name: 'Semestre 2',
+        year: 2030,
+      },
+    ],
+  }
+}
+</script>
+
+<template>
+  <Story
+    title="UiTimeline"
+    :layout="{
+      type: 'grid',
+      width: 1350,
+    }"
+    group="homepage"
+    :init-state="initState"
+  >
+    <template #default="{ state }">
+      <UiTimeline :keydates="state.keydates"></UiTimeline>
+    </template>
+  </Story>
+</template>
