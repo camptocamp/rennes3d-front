a7bf75bf1c429a06372091134b28c184ffca9f74
Ismail Sunni <imajimatika@gmail.com>
2022-10-18 08:22:09 +0200

    Adding style function.

--- src/components/map/OLMapComponent.vue
+++ src/components/map/OLMapComponent.vue
@@ -12,6 +12,8 @@ import { getWidth } from 'ol/extent'
 import GeoJSON from 'ol/format/GeoJSON'
 import { Vector as VectorSource } from 'ol/source'
 import { Vector as VectorLayer } from 'ol/layer'
+import { Style, Stroke } from 'ol/style'
+import type { Feature } from 'ol'
 
 const resolutions = []
 const matrixIds = []
@@ -41,11 +43,69 @@ const rennesBaseMap = new TileLayer({
   }),
 })
 
+// Styles
+const styles: { [styleName: string]: Style } = {
+  unStarted: new Style({
+    stroke: new Stroke({
+      color: '#000000',
+      width: 3,
+      lineDash: [5],
+    }),
+  }),
+  underConstruction: new Style({
+    stroke: new Stroke({
+      color: '#D7191C',
+      width: 3,
+    }),
+  }),
+  constructionFinished: new Style({
+    stroke: new Stroke({
+      color: '#FDBF6F',
+      width: 3,
+    }),
+  }),
+  commisioning: new Style({
+    stroke: new Stroke({
+      color: '#04B200',
+      width: 3,
+    }),
+  }),
+}
+
+function getStyleName(feature: Feature): string {
+  // TODO: Do it better, and compare with the selected time
+  const property = String(feature.getProperties()['en_cours_t'])
+  if (property.startsWith('2025')) {
+    return 'commisioning'
+  }
+  if (property.startsWith('2026')) {
+    return 'constructionFinished'
+  }
+  if (property.startsWith('2027')) {
+    return 'constructionFinished'
+  }
+  if (property.startsWith('2028')) {
+    return 'underConstruction'
+  }
+  if (property.startsWith('2029')) {
+    return 'underConstruction'
+  }
+  if (property.startsWith('2030')) {
+    return 'unStarted'
+  }
+  return 'unStarted'
+}
+
+const styleFunction = function (feature: Feature): Style {
+  return styles[getStyleName(feature)]
+}
+
 const planningLayer = new VectorLayer({
   source: new VectorSource({
     url: 'https://gist.githubusercontent.com/ismailsunni/561f39f97f8e1a36491207a61224270c/raw/b477374024d797785eea7e9cc23d01766e3812f5/planning_rm.geojson',
     format: new GeoJSON(),
   }),
+  style: styleFunction,
 })
 
 let map: Ref<Map | undefined> = ref(undefined)
