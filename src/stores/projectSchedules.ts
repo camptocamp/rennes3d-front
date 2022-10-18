4aacf8928c76b2759248249cac9e8fd3e6ae69a5
Ismail Sunni <imajimatika@gmail.com>
2022-10-18 10:46:44 +0200

    use semester instead of month.

--- src/stores/projectSchedules.ts
+++ src/stores/projectSchedules.ts
@@ -4,15 +4,16 @@ import { defineStore } from 'pinia'
 
 export const useProjectSchedulesStore = defineStore('projectSchedules', () => {
   const selectedYear: Ref<number> = ref(2029)
-  const selectedMonth: Ref<number> = ref(1)
+  const selectedSemester: Ref<number> = ref(1)
 
   function getSelectedDate() {
-    return new Date(selectedYear.value, selectedMonth.value)
+    const selectedMonth = selectedSemester.value % 2 == 1 ? 1 : 7
+    return new Date(selectedYear.value, selectedMonth)
   }
 
   return {
     getSelectedDate,
-    selectedMonth,
+    selectedSemester,
     selectedYear,
   }
 })
