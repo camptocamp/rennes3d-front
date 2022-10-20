5747ad43b12337c8f222bbca793c8daabb143880
Abdelmalek <ab.khelouat@gmail.com>
2022-10-20 09:09:17 +0200

    feat(map): adding timeline to the planning view

--- src/views/PlanningView.vue
+++ src/views/PlanningView.vue
@@ -1,10 +1,20 @@
 <script setup lang="ts">
 import PlanningMapComponent from '../components/map/PlanningMapComponent.vue'
 import UiButton from '../components/ui/UiButton.vue'
+import Timeline from '../components/ui/UiTimeline.vue'
 import { ArrowLeftIcon } from '@heroicons/vue/20/solid'
 import { usePanelsStore } from '@/stores/panels'
+import { timeLineFixtures } from '../model/timeLineItems.fixtures'
+import { usePlanningStore } from '@/stores/planning'
 
 const panelStore = usePanelsStore()
+
+const planningStore = usePlanningStore()
+
+const timeLineItems = timeLineFixtures()
+const updateDate = (date: Date) => {
+  planningStore.setDate(date)
+}
 </script>
 
 <template>
@@ -21,7 +31,7 @@ const panelStore = usePanelsStore()
       <div class="font-poppins font-semibold text-xl w-72">
         Planning d’aménagement du réseau trambus
       </div>
-      <div>Timeline</div>
+      <div><Timeline :items="timeLineItems" @current-date="updateDate" /></div>
     </div>
     <div class="flex grow relative">
       <PlanningMapComponent></PlanningMapComponent>
