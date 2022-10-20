793c7d86f759f4ac3755b0177a810df94392b9a1
Abdelmalek <ab.khelouat@gmail.com>
2022-10-20 09:03:20 +0200

    feat(ui): add timeLineItems fixtures

---
+++ src/model/timeLineItems.fixtures.ts
@@ -0,0 +1,64 @@
+import type { TimeLineItem } from './timeLineItems.model'
+
+export const timeLineFixtures = (): TimeLineItem[] => [
+  {
+    name: 'Semestre 1',
+    semester: 1,
+    year: 2025,
+  },
+  {
+    name: 'Semestre 2',
+    semester: 2,
+    year: 2025,
+  },
+  {
+    name: 'Semestre 1',
+    semester: 1,
+    year: 2026,
+  },
+  {
+    name: 'Semestre 2',
+    semester: 2,
+    year: 2026,
+  },
+  {
+    name: 'Semestre 1',
+    semester: 1,
+    year: 2027,
+  },
+  {
+    name: 'Semestre 2',
+    semester: 2,
+    year: 2027,
+  },
+  {
+    name: 'Semestre 1',
+    semester: 1,
+    year: 2028,
+  },
+  {
+    name: 'Semestre 2',
+    semester: 2,
+    year: 2028,
+  },
+  {
+    name: 'Semestre 1',
+    semester: 1,
+    year: 2029,
+  },
+  {
+    name: 'Semestre 2',
+    semester: 2,
+    year: 2029,
+  },
+  {
+    name: 'Semestre 1',
+    semester: 1,
+    year: 2030,
+  },
+  {
+    name: 'Semestre 2',
+    semester: 2,
+    year: 2030,
+  },
+]
