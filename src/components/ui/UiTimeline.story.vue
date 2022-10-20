1313acc18a62a7dcdead666d2c0560a537c3d410
Abdelmalek <ab.khelouat@gmail.com>
2022-10-20 09:20:44 +0200

    feat(story): adding a second variant to timeline component

--- src/components/ui/UiTimeline.story.vue
+++ src/components/ui/UiTimeline.story.vue
@@ -5,6 +5,33 @@ import { timeLineFixtures } from '../../model/timeLineItems.fixtures'
 function initState() {
   return {
     items: timeLineFixtures(),
+    itemsPrototype: [
+      {
+        name: 'Semestre 1',
+        semester: 1,
+        year: 2025,
+      },
+      {
+        name: 'Semestre 2',
+        semester: 2,
+        year: 2025,
+      },
+      {
+        name: 'Semestre 1',
+        semester: 1,
+        year: 2026,
+      },
+      {
+        name: 'Semestre 2',
+        semester: 2,
+        year: 2026,
+      },
+      {
+        name: 'Semestre 1',
+        semester: 1,
+        year: 2027,
+      },
+    ],
   }
 }
 </script>
@@ -17,10 +44,16 @@ function initState() {
       width: 1350,
     }"
     group="homepage"
-    :init-state="initState"
   >
-    <template #default="{ state }">
-      <UiTimeline :items="state.items"></UiTimeline>
-    </template>
+    <Variant title="Current" :init-state="initState">
+      <template #default="{ state }">
+        <UiTimeline :items="state.items"></UiTimeline>
+      </template>
+    </Variant>
+    <Variant title="With diffrent items" :init-state="initState">
+      <template #default="{ state }">
+        <UiTimeline :items="state.itemsPrototype"></UiTimeline>
+      </template>
+    </Variant>
   </Story>
 </template>
