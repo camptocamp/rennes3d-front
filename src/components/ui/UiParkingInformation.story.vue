fb5b2effb35f3fe473df9719fd45fae7c6bc947b
Ismail Sunni <imajimatika@gmail.com>
2022-10-26 08:04:37 +0700

    Initialize parking component.

---
+++ src/components/ui/UiParkingInformation.story.vue
@@ -0,0 +1,23 @@
+<script lang="ts" setup>
+function initState() {
+  return {
+    check: true,
+  }
+}
+</script>
+
+<template>
+  <Story
+    title="UiParkingInformation"
+    :layout="{
+      type: 'grid',
+      width: 450,
+    }"
+    group="homepage"
+    :init-state="initState"
+  >
+    <template #default>
+      <UiParkingInformation> </UiParkingInformation>
+    </template>
+  </Story>
+</template>
---
+++ src/components/ui/UiParkingInformation.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts"></script>
+<template>
+  <div class="">
+    <div>Icon</div>
+    <div class="">
+      <div>Name</div>
+      <div>Station</div>
+      <div>Address</div>
+    </div>
+    <div class="">
+      <div>300</div>
+      <div>places</div>
+    </div>
+  </div>
+</template>
