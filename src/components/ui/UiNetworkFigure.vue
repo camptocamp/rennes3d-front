03c075a727082bb11483d8e2ed18cef3d8540e1d
Ismail Sunni <imajimatika@gmail.com>
2022-11-02 12:41:37 +0700

    Use a specific span for doing custom style

--- src/components/ui/UiNetworkFigure.vue
+++ src/components/ui/UiNetworkFigure.vue
@@ -11,9 +11,9 @@ const props = defineProps({
 <template>
   <div class="flex flex-col p-0 font-dm-sans">
     <div class="flex items-center p-0 gap-2">
-      <div class="leading-8 font-bold grow">
-        <span class="text-2xl">{{ props.figure }}</span>
-        <span class="text-base leading-8 pl-1">{{ props.unit }}</span>
+      <div class="leading-8 font-bold grow text-2xl">
+        {{ props.figure }}
+        <span class="text-base">{{ props.unit }}</span>
       </div>
       <div v-if="props.icon">
         <img class="w-9 h-9" :src="props.icon" />
