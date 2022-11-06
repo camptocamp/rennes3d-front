f13e17fe22db23c60bfcc38ef77a3e5f67ae890c
Ismail Sunni <imajimatika@gmail.com>
2022-11-06 21:03:15 +0700

    Reorder the component and remove default tailwind classes.

--- src/components/ui/UiTravelTime.vue
+++ src/components/ui/UiTravelTime.vue
@@ -16,12 +16,12 @@ const props = defineProps({
 </script>
 <template>
   <div
-    class="flex flex-row items-center px-0 py-12 gap-2.5 w-80 h-20 bg-neutral-200 rounded-md"
+    class="flex items-center px-0 py-12 gap-2.5 w-80 h-20 bg-neutral-200 rounded-md"
   >
     <div
-      class="box-border flex flex-col justify-center items-start p-0 w-24 h-20 border-r border-r-neutral-300 border-solid"
+      class="box-border flex flex-col justify-center items-start p-0 w-24 h-20"
     >
-      <div class="h-8 w-6 flex gap-1 items-baseline flex-row pl-3">
+      <div class="h-8 w-6 flex gap-1 items-baseline pl-3">
         <div class="text-sm leading-8 font-bold font-poppins">
           {{ props.newDuration }}
         </div>
@@ -29,15 +29,14 @@ const props = defineProps({
       </div>
       <div class="flex flex-col items-start h-7 w-16 pl-3">
         <div
-          class="w-20 h-4 font-poppins not-italic font-medium text-xs leading-4 bg-neutral-200"
+          class="w-20 h-4 font-poppins font-medium text-xs leading-4 bg-neutral-200"
         >
           au lie de {{ props.oldDuration }}
         </div>
       </div>
     </div>
-    <IconLine :line="lineNumber" :size="'l'"></IconLine>
     <div
-      class="flex flex-row items-start p-0 gap-5 w-9 h-2 rounded origin-center rotate-90"
+      class="flex items-start p-0 gap-5 w-9 h-2 rounded origin-center rotate-90"
       :class="'bg-t' + props.lineNumber + '-line'"
     >
       <div
@@ -49,17 +48,14 @@ const props = defineProps({
     </div>
     <div class="flex flex-col items-start p-0 gap-1 w-28 h-12">
       <div class="flex flex-col justify-center items-start p-0 gap-1 w-28 h-12">
-        <div
-          class="w-28 h-5 font-poppins not-italic font-medium text-sm leading-5 text-black"
-        >
+        <div class="w-28 h-5 font-poppins font-medium text-sm leading-5">
           {{ props.startStation }}
         </div>
-        <div
-          class="w-28 h-5 font-poppins not-italic font-medium text-sm leading-5 text-black"
-        >
+        <div class="w-28 h-5 font-poppins font-medium text-sm leading-5">
           {{ props.endStation }}
         </div>
       </div>
     </div>
+    <IconLine :line="lineNumber" :size="'l'"></IconLine>
   </div>
 </template>
