ee37cda45ce463efb764a5f4f6c1c9a67e7050b4
Ismail Sunni <imajimatika@gmail.com>
2022-11-06 20:32:59 +0700

    Better way styling based on props.

--- src/components/ui/icons/IconLine.vue
+++ src/components/ui/icons/IconLine.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
 import { computed, type PropType } from 'vue'
+
 type LineNumber = 1 | 2 | 3 | 4
 type LineIconSize = 's' | 'm' | 'l' | 'xl'
+
 const props = defineProps({
   line: {
     type: Number as PropType<LineNumber>,
@@ -10,31 +12,34 @@ const props = defineProps({
   size: String as PropType<LineIconSize>,
 })
 
-const baseColor = ['indigo', 'pink', 'emerald', 'purple']
-
 const lineText = computed(() => {
   if (props.size == 's') return props.line
   else return `T${props.line}`
 })
-
-const backgroundColor = computed(() => {
-  return baseColor[props.line - 1]
-    ? 'bg-' + baseColor[props.line - 1] + '-600'
-    : 'bg-black'
-})
-
-// const textClass = computed(() => {
-//   if (props.size == 's') {
-//     return ''
-//   }
-// })
 </script>
 <template>
   <div
-    class="flex items-center w-12 h-12 rounded-full justify-center"
-    :class="backgroundColor"
+    class="flex items-center rounded-full justify-center"
+    :class="{
+      'w-4 h-4': $props.size === 's',
+      'w-6 h-6': $props.size === 'm',
+      'w-8 h-8': $props.size === 'l',
+      'w-12 h-12': $props.size === 'xl',
+      'bg-indigo-600': $props.line == 1,
+      'bg-pink-600': $props.line == 2,
+      'bg-emerald-600': $props.line == 3,
+      'bg-purple-600': $props.line == 4,
+    }"
   >
-    <div class="font-dm-sans font-bold text-white text-xl items-end">
+    <div
+      class="font-dm-sans font-bold text-white text-xl items-end"
+      :class="{
+        'font-normal text-xs': $props.size === 's',
+        'font-bold text-xs': $props.size === 'm',
+        'font-bold text-lg': $props.size === 'l',
+        'font-bold text-xl': $props.size === 'xl',
+      }"
+    >
       {{ lineText }}
     </div>
   </div>
