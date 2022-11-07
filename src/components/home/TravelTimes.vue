835fbc8597c9d9561ef4245833fd3e5256b4bb72
Ismail Sunni <imajimatika@gmail.com>
2022-11-07 10:12:51 +0700

    Apply layout per the new design.

--- src/components/home/TravelTimes.vue
+++ src/components/home/TravelTimes.vue
@@ -15,20 +15,30 @@ onMounted(async () => {
 </script>
 
 <template>
-  <div class="font-dm-sans font-bold text-lg leading-6 text-neutral-400">
-    Vos temps de parcours bientot réduits
+  <div class="flex items-center p-0 gap-2.5">
+    <div class="flex flex-col items-start gap-3 pt-0 pr-9 pb-0 pl-0">
+      <div class="font-dm-sans font-bold text-lg leading-6">
+        Vos futurs temps de parcours
+      </div>
+      <div class="flex flex-row items-start p-0 gap-3 overflow-x-scroll">
+        <UiTravelTime
+          v-for="travelTime in state.travelTimes"
+          :key="travelTime.line"
+          :newDuration="travelTime.new"
+          :oldDuration="travelTime.old"
+          :lineNumber="travelTime.line"
+          :startStation="travelTime.start"
+          :endStation="travelTime.end"
+        >
+        </UiTravelTime>
+      </div>
+      <div class="flex items-center gap-1 pt-0 pb-1 pl-0 pr-0">
+        <div class="font-dm-sans font-medium text-base">
+          <a href="">Voir plus</a>
+        </div>
+        <!-- TODO: create the link component with the button -->
+      </div>
+    </div>
+    <!-- TODO: Add button to scroll  -->
   </div>
-  <div class="flex flex-row items-start p-0 gap-3 overflow-x-auto">
-    <UiTravelTime
-      v-for="travelTime in state.travelTimes"
-      :key="travelTime.line"
-      :newDuration="travelTime.new"
-      :oldDuration="travelTime.old"
-      :lineNumber="travelTime.line"
-      :startStation="travelTime.start"
-      :endStation="travelTime.end"
-    >
-    </UiTravelTime>
-  </div>
-  <a href="">Voir plus</a>
 </template>
