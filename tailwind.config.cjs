b7f7d4bad083cdaff120bb87fb5ae601eb470439
Ismail Sunni <imajimatika@gmail.com>
2022-11-10 09:09:46 +0700

    Remove unused color.

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,10 +2,6 @@
 module.exports = {
   content: ['index.html', './src/**/*.{html,js,ts,vue}'],
   safelist: [
-    'bg-t1-line',
-    'bg-t2-line',
-    'bg-t3-line',
-    'bg-t4-line',
     'bg-indigo-100',
     'bg-pink-100',
     'bg-emerald-100',
@@ -38,10 +34,6 @@ module.exports = {
         'dm-sans': ['DM Sans'],
       },
       colors: {
-        't1-line': '#D18725',
-        't2-line': '#DF51A9',
-        't3-line': '#61D03F',
-        't4-line': '#68E3DF',
         'gray-dark': '#5E5E5E',
       },
     },
