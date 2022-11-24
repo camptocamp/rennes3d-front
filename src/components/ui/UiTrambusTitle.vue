ee6184249c26dc353d3da45e89cd768a7cce2812
Ismail Sunni <imajimatika@gmail.com>
2022-11-24 17:48:54 +0700

    Update style for icon, title, and description.

--- src/components/ui/UiTrambusTitle.vue
+++ src/components/ui/UiTrambusTitle.vue
@@ -3,14 +3,12 @@ import UiTrambusLogo from './UiTrambusLogo.vue'
 </script>
 
 <template>
-  <div class="flex flex-col items-start p-0 gap-2 w-4/5 h-28 font-poppins">
-    <div class="flex flex-row items-start p-0 gap-2 h-14">
+  <div class="flex flex-col p-0 gap-3 h-28 font-dm-sans">
+    <div class="flex items-center p-0 gap-2.5">
       <UiTrambusLogo></UiTrambusLogo>
-      <div class="font-bold text-4xl w-80 h-14 justify-center flex flex-col">
-        Reseau Trambus
-      </div>
+      <div class="font-bold text-3xl text-center">Réseau Trambus</div>
     </div>
-    <div class="font-light text-base leading-6">
+    <div class="font-medium text-sm text-neutral-800">
       Découvrez et contribuez au futur réseau de transport de Rennes Métropole
     </div>
   </div>
