125745e1629fdd0ced538b4d46b286077a27ec61
Ismail Sunni <imajimatika@gmail.com>
2022-11-26 06:44:59 +0700

    Add link with animation on footer.

--- src/components/home/FooterArea.vue
+++ src/components/home/FooterArea.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
 import UiRennesLogo from '@/components/ui/UiRennesLogo.vue'
 import UiDivider from '../ui/UiDivider.vue'
-// import UiLinkMetis from '../ui/UiLinkMetis.vue'
 </script>
 <template>
   <div class="flex flex-col p-0 gap-4 font-dm-sans">
@@ -13,19 +12,22 @@ import UiDivider from '../ui/UiDivider.vue'
       </div>
     </div>
     <UiDivider></UiDivider>
-    <div>
-      <!-- <div
-        class="font-normal text-xs text-neutral-500 before:w-0 before:h-px before:absolute before:bottom-0 before:right-0 before:transition-all before:duration-300 before:bg-neutral-500 hover:before:w-full hover:before:left-0 underline hover:no-underline"
-      >
-        Mentions légales
-      </div> -->
-      <!-- <div>Politique de protection des données personnelles</div> -->
-      <!-- <UiLinkMetis
-        :hasArrow="false"
-        class="grow-0 text-neutral-500 font-normal text-xs"
-        :underlineColor="'bg-neutral-500'"
-        >Mentions légales</UiLinkMetis
-      > -->
+    <!-- TODO: Refactor these links as a separated component -->
+    <div class="flex gap-3 justify-center">
+      <div class="relative w-fit">
+        <div
+          class="font-normal text-xs text-neutral-500 before:w-0 before:h-px before:absolute before:bottom-0 before:right-0 before:transition-all before:duration-300 before:bg-neutral-500 hover:before:w-full hover:before:left-0"
+        >
+          Mentions légales
+        </div>
+      </div>
+      <div class="relative w-fit">
+        <div
+          class="font-normal text-xs text-neutral-500 before:w-0 before:h-px before:absolute before:bottom-0 before:right-0 before:transition-all before:duration-300 before:bg-neutral-500 hover:before:w-full hover:before:left-0"
+        >
+          Politique de protection des données personnelles
+        </div>
+      </div>
     </div>
   </div>
 </template>
