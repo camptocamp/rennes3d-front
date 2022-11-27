2fadc4518d994f94b3b787c8eb68ed34930c0ee3
Ismail Sunni <imajimatika@gmail.com>
2022-11-27 17:14:52 +0700

    Use Control Panel Button for gallery panel.

--- src/components/ui/UiPhotoGalery.vue
+++ src/components/ui/UiPhotoGalery.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
 import type { PropType } from 'vue'
-import { ChevronDownIcon } from '@heroicons/vue/20/solid'
-import { ChevronUpIcon } from '@heroicons/vue/20/solid'
+import UiPanelControlButton from './UiPanelControlButton.vue'
 
 const emit = defineEmits(['toggleEvent'])
 
@@ -22,13 +21,11 @@ function sendEvent() {
 </script>
 <template>
   <div class="flex flex-col items-center justify-end p-0">
-    <button
+    <UiPanelControlButton
+      :is-open="galleryShown"
+      :anchor-position="'bottom'"
       @click="sendEvent"
-      class="flex flex-row justify-center items-center px-2 py-4 gap-4 w-11 h-7 bg-white rounded-t-xl"
-    >
-      <ChevronDownIcon :class="{ hidden: !galleryShown }"></ChevronDownIcon>
-      <ChevronUpIcon :class="{ hidden: galleryShown }"></ChevronUpIcon>
-    </button>
+    ></UiPanelControlButton>
     <div
       class="flex flex-row items-start p-3 gap-3 w-auto h-56 bg-white rounded-l-xl rounded-r-xl rounded-t-none rounded-b-none"
       :class="{ hidden: !galleryShown }"
