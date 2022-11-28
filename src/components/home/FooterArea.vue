5a58c96b1f1a279b909d3ff54e65dbb3a0c4f7ac
Ismail Sunni <imajimatika@gmail.com>
2022-11-28 14:06:34 +0700

    Tidy up horizontal line for footer.

--- src/components/home/FooterArea.vue
+++ src/components/home/FooterArea.vue
@@ -1,10 +1,11 @@
 <script setup lang="ts">
 import UiRennesLogo from '@/components/ui/UiRennesLogo.vue'
-import UiDivider from '@/components/ui/UiDivider.vue'
 import UiLinkFooter from '@/components/ui/UiLinkFooter.vue'
 </script>
 <template>
-  <footer class="flex flex-col p-0 gap-4 font-dm-sans">
+  <footer
+    class="flex flex-col p-0 gap-4 font-dm-sans divide-y divide-neutral-200"
+  >
     <div class="flex p-0 gap-6">
       <UiRennesLogo class="w-36 h-7"></UiRennesLogo>
       <div class="font-normal text-xs">
@@ -12,7 +13,6 @@ import UiLinkFooter from '@/components/ui/UiLinkFooter.vue'
         3D.
       </div>
     </div>
-    <UiDivider></UiDivider>
     <div class="flex gap-3 justify-center">
       <UiLinkFooter :url="'https://metropole.rennes.fr/mentions-legales'"
         >Mentions légales</UiLinkFooter
