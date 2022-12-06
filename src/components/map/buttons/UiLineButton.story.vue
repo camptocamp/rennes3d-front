77c2909991f0fce1189363650635c46e4420b0ab
Ismail Sunni <imajimatika@gmail.com>
2022-12-06 14:23:42 +0700

    Start line button component and its story.

---
+++ src/components/map/buttons/UiLineButton.story.vue
@@ -0,0 +1,35 @@
+<script lang="ts" setup>
+import UiLineButton from './UiLineButton.vue'
+</script>
+
+<template>
+  <Story
+    title="Line Buttons"
+    :layout="{
+      type: 'grid',
+      width: 200,
+    }"
+    group="ui-base"
+  >
+    <Variant title="Small">
+      <template #default>
+        <UiLineButton :line="1"></UiLineButton>
+      </template>
+    </Variant>
+    <!-- <Variant title="Medium">
+      <template #default>
+        <IconLine :line="2" :size="'m'"> </IconLine>
+      </template>
+    </Variant>
+    <Variant title="Large">
+      <template #default>
+        <IconLine :line="3" :size="'l'"> </IconLine>
+      </template>
+    </Variant>
+    <Variant title="Extra Large">
+      <template #default>
+        <IconLine :line="4" :size="'xl'"> </IconLine>
+      </template>
+    </Variant> -->
+  </Story>
+</template>
---
+++ src/components/map/buttons/UiLineButton.vue
@@ -0,0 +1,27 @@
+<script lang="ts" setup>
+import type { LineNumber } from '@/model/lines.model'
+import { computed, type PropType } from 'vue'
+import { getColor } from '@/services/color'
+
+const props = defineProps({
+  line: {
+    type: Number as PropType<LineNumber>,
+    required: true,
+  },
+  active: {
+    type: Boolean,
+    default: false,
+  },
+})
+
+const buttonStyle = computed(() => {
+  const bgColor = getColor('bg', props.line, 600)
+  return [bgColor]
+})
+</script>
+
+<template>
+  <button :class="buttonStyle">
+    {{ 'T' + props.line }}
+  </button>
+</template>
