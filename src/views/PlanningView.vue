91baa9fe5c1bb5d2f18ff40b325ff1ca448a5bc0
Ismail Sunni <imajimatika@gmail.com>
2022-12-20 21:40:07 +0700

    Rework back button on planning view.

--- src/views/PlanningView.vue
+++ src/views/PlanningView.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
 import PlanningMapComponent from '../components/map/PlanningMapComponent.vue'
 import UiButton from '../components/ui/UiButton.vue'
-import { ArrowLeftIcon } from '@heroicons/vue/20/solid'
 import { usePanelsStore } from '@/stores/panels'
 import TimelineController from '@/components/home/TimelineController.vue'
+import ChevronArrowRight from '@/assets/icons/chevron-left.svg'
 
 const panelStore = usePanelsStore()
 </script>
@@ -11,14 +11,12 @@ const panelStore = usePanelsStore()
 <template>
   <main class="h-screen flex flex-col">
     <div class="h-1/6 p-4 bg-white flex flex-row gap-2 items-center">
-      <div>
-        <UiButton
-          class="shadow-sm"
-          @click="panelStore.isPlanningViewShown = false"
-        >
-          <ArrowLeftIcon class="w-6 h-6"></ArrowLeftIcon>
-        </UiButton>
-      </div>
+      <UiButton
+        class="shadow-md rounded-lg p-2 flex gap-2.5"
+        @click="panelStore.isPlanningViewShown = false"
+      >
+        <img :src="ChevronArrowRight" />
+      </UiButton>
       <div class="font-poppins font-semibold text-xl w-72">
         Planning d'aménagement du réseau trambus
       </div>
